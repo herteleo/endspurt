e1c419877bc89b32f432d7382e62296cf036d7ec
herteleo <leonard.hertel@gmail.com>
2019-11-25 01:59:03 +0100

    ci: fix release commit message format

--- .releaserc.js
+++ .releaserc.js
@@ -6,7 +6,10 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/git',
+    ['@semantic-release/git', {
+      // eslint-disable-next-line no-template-curly-in-string
+      message: 'chore: release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    }],
     '@semantic-release/github',
   ],
 };
